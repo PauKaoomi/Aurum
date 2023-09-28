4df7ec30e2000f7f1f89be7ad54a26359e060a88
Pauline Kaoomi <pauav22015@gmail.com>
2023-09-27 21:59:29 -0500

    funcion caja de comentarios

--- javascript/scriptmain.js
+++ javascript/scriptmain.js
@@ -2,9 +2,15 @@
 
 function publicar(){
     let textoPub = window.document.querySelector("#cuadropub").value
-    let CajaComentario = "<article class='comentariosPub'><article class='espacioImg'><img class='ImgComentarios' src='Img/concierto.png' alt='Imagen Redonda' class='imagen-redonda'></article><article class='espacioCajaComentario'><p>"+textoPub+"</p></article></article>"
-    const comentario = document.createElement('p')
-    document.getElementById("comentariosCaja").appendChild(CajaComentario)
+
+    const comentario = document.createElement('article')
+    comentario.className += "comentariosPub"
+    comentario.innerHTML = "<article class='espacioImg'><img class='ImgComentarios' src='Img/concierto.png' alt='Imagen Redonda' class='imagen-redonda'></article><article class='espacioCajaComentario'><p class='usuario'><b>@usuario</b></p><p>"+textoPub+"</p></article>"
+
+    const padre = document.querySelector('#comentariosCaja')
+
+    padre.appendChild(comentario)
+    
 }
 
 let btnPublicar = window.document.querySelector("#buttonpubuno") 
