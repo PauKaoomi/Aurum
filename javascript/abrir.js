35877e980d56c77305c553c2856a482574cd711f
FSFCR16 <Fajarfsabo161@gmail.com>
2023-09-29 00:27:13 -0500

    javasss

--- javascript/abrir.js
+++ javascript/abrir.js
@@ -1,71 +1,72 @@
 let listaClick = document.querySelectorAll(".click")
+let listaVentAbrir=document.querySelectorAll(".abrirV")
+let listaCerrar=document.querySelectorAll(".cerrarV")
 
 
-
+console.log(listaVentAbrir)
 
 //function eventC(id){
+
 
 for (i = 0; i < listaClick.length; i++) {
 
     listaClick[i].addEventListener("click", function (e) {
 
+
         const ids = e.target.getAttribute("id-modal");
+        console.log(ids)
 
-        for (i = 0; i < listaClick.length; i++) {
+        for (i = 0; i < listaVentAbrir.length; i++) {
 
-            let idd = listaClick[i].getAttribute("id-modal")
+            let idd = listaVentAbrir[i].getAttribute("id")
 
+            console.log(ids)
+            console.log(idd)
 
-            if (ids == idd) {
+            
+            if (ids == idd ) {
 
 
                 document.getElementById(idd).style.display = "block"
-            } else {
+
+
+
+            }else {
 
                 document.getElementById(idd).style.display = "none"
+                document.getElementById("mainCerrar").style.display="none"
+                document.getElementById("asideC").style.display="none"
+                document.getElementById("asideDosC").style.display="none"
+                document.getElementById("navC").style.display="none"
+
+
+            }if (ids=="x"){
+            
+                console.log("Se ejecuto")
+                document.getElementById("mainCerrar").style.display="block"
+                document.getElementById("asideC").style.display="block"
+                document.getElementById("asideDosC").style.display="block"
+                document.getElementById("navC").style.display="block"
+
+
             }
+
         }
-
-
-
     })
 
 }
 
 
-function cerrar(){
-    for (i = 0; i < listaClick.length; i++) {
 
-        listaClick[i].addEventListener("click", function (e) {
-
-            const ids = e.target.getAttribute("id-modal");
-
-            for (i = 0; i < listaClick.length; i++) {
-
-                let idd = listaClick[i].getAttribute("id-modal")
-
-
-                if (ids == idd) {
-
-
-                    document.getElementById(idd).style.display = "none";
-                } 
-
-            }
-        })
-    }
-}
-
-
-    /*document.querySelectorAll(".click").forEach(function (idEtiqueta) {
-    
-        idEtiqueta.addEventListener("click", function (e) {
-    
-            const id = e.target.getAttribute("id");
-    
-        });
-    
-    });*/
+/*document.querySelectorAll(".click").forEach(function (idEtiqueta) {
+ 
+    idEtiqueta.addEventListener("click", function (e) {
+ 
+        const id = e.target.getAttribute("id");
+ 
+    });
+ 
+});*/
 
 
 /*    function abrir(indicador) {
@@ -114,56 +115,62 @@ function cerrar(){
 
     }*/
 
-    //Iniciar sesion
-    let listaContraseña = ["santiago", "sebastian", "paula", "alvaro", "rafael"]
-    let listaCorreo = ["santiago.aurum@gmail.com", "sebastian.aurum@gmail.com", "paula.aurum@gmail.com", "alvaro.aurum@gmail.com", "rafael.aurum@gmail.com"]
-    let btnIni = window.document.querySelector("#botonIniciar")
-    let ultimo = listaCorreo[listaCorreo.length - 1]
+//Iniciar sesion
+let listaContraseña = ["santiago", "sebastian", "paula", "alvaro", "rafael"]
+let listaCorreo = ["santiago.aurum@gmail.com", "sebastian.aurum@gmail.com", "paula.aurum@gmail.com", "alvaro.aurum@gmail.com", "rafael.aurum@gmail.com"]
+let btnIni = window.document.querySelector("#botonIniciar")
+let ultimo = listaCorreo[listaCorreo.length - 1]
 
-    function correoI() {
+function correoI() {
 
-        let input = document.querySelector("#inp-correo").value;
-        let correo = false
+    let input = document.querySelector("#inp-correo").value;
+    let correo = false
 
-        if (listaCorreo.includes(input)) {
-            correo = true
+    if (listaCorreo.includes(input)) {
+        correo = true
 
-        }
-        else if (!listaCorreo.includes(input)) {
-            correo = false
+    }
+    else if (!listaCorreo.includes(input)) {
+        correo = false
 
-        }
-
-        return correo
     }
 
-    function contraseña() {
+    return correo
+}
 
-        let input = document.querySelector("#cont").value;
-        let contra = false
+function contraseña() {
 
-        if (listaContraseña.includes(input)) {
-            contra = true
-        }
-        else if (!listaContraseña.includes(input)) {
-            contra = false
-        }
+    let input = document.querySelector("#cont").value;
+    let contra = false
 
-        return contra
+    if (listaContraseña.includes(input)) {
+        contra = true
+    }
+    else if (!listaContraseña.includes(input)) {
+        contra = false
     }
 
-    function acceso() {
-        if (contraseña() == true && contraseña() == true) {
+    return contra
+}
 
-            document.querySelector(".inicio-registro").style.display = "none";
-            document.querySelector(".buscar").style.display = "block";
-            document.querySelector(".globo-perfil").style.display = "block";
-            document.querySelector("#emergente").style.display = "none";
+function acceso() {
+    if (contraseña() == true && contraseña() == true) {
 
-        } else {
-            alert("La contraseña o el correo no coincide")
-        }
+        document.querySelector(".inicio-registro").style.display = "none";
+        document.querySelector(".buscar").style.display = "block";
+        document.querySelector(".globo-perfil").style.display = "block";
+        document.querySelector(".pubPost").style.display = "block"
+        document.getElementById("mainCerrar").style.display="block"
+        document.getElementById("asideC").style.display="block"
+        document.getElementById("asideDosC").style.display="block"
+        document.getElementById("navC").style.display="block"
+        document.querySelector("#emergente").style.display = "none";
+    
+
+    } else {
+        alert("La contraseña o el correo no coincide")
     }
-    function eventoR() {
-        btnIni.addEventListener("click", acceso)
-    }
+}
+function eventoR() {
+    btnIni.addEventListener("click", acceso)
+}
