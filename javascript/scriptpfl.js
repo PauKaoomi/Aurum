73c7bebd277b169e2d957c83b98005f9897821e7
sebas1acero <sebas1acero@gmail.com>
2023-10-03 22:14:11 -0500

    cuarta entrega

--- javascript/scriptpfl.js
+++ javascript/scriptpfl.js
@@ -1,47 +1,66 @@
 
-const boton0 = document.getElementById("boton0");
-const boton1 = document.getElementById("boton1");
-const boton2 = document.getElementById("boton2");
-const boton3 = document.getElementById("boton3");
-const boton4 = document.getElementById("iniciosebas");
+const boton0 = document.getElementById("botonverperfil");
+const boton1 = document.getElementById("botonpost");
+const boton2 = document.getElementById("botoncitas");
+const boton3 = document.getElementById("botonlikes");
+const boton4 = document.getElementById("botoninicio");
 
-const contenido0 = document.getElementById("contenidoprincipal");
-const contenido1 = document.getElementById("contenido1");
-const contenido2 = document.getElementById("contenido2");
-const contenido3 = document.getElementById("contenido3");
-const contenido4 = document.getElementById("perfilsebas");
+const contenido0 = document.getElementById("miperfil");
+const contenido1 = document.getElementById("contenidoprincipal");
+const contenido2 = document.getElementById("contenidocitas");
+const contenido3 = document.getElementById("contenidolikes");
 
-contenido4.style.display ="none"
+contenido1.style.display = "contents";
+contenido0.style.display = "none";
 contenido2.style.display = "none";
 contenido3.style.display = "none";
 
-
 boton0.addEventListener("click", function () {
-    contenido4.style.display="block"
-    contenido0.style.display = "none";
-
-});
-
-boton4.addEventListener("click", function () {
-    contenido4.style.display="none"
     contenido0.style.display = "block";
-
-});
-
-boton1.addEventListener("click", function () {
     contenido1.style.display = "block";
     contenido2.style.display = "none";
     contenido3.style.display = "none";
+
+
+
+});
+
+
+boton1.addEventListener("click", function () {
+
+    contenido1.style.display = "block";
+    contenido2.style.display = "none";
+    contenido3.style.display = "none";
+
+
 });
 
 boton2.addEventListener("click", function () {
+
     contenido1.style.display = "none";
     contenido2.style.display = "grid";
     contenido3.style.display = "none";
 });
 
 boton3.addEventListener("click", function () {
+
     contenido1.style.display = "none";
     contenido2.style.display = "none";
     contenido3.style.display = "grid";
 });
+
+boton4.addEventListener("click", function () {
+
+    contenido0.style.display = "none";
+    contenido1.style.display = "contents";
+    contenido2.style.display = "none";
+    contenido3.style.display = "none";
+
+
+});
+
+
+
+
+
+
